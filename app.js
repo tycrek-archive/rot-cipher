c02d53df0622f0c4b712019f4771ff5bf8642820
tycrek <josh.moore@jmoore.dev>
2019-10-15 14:46:19 -0600

    Added getShift function

--- app.js
+++ app.js
@@ -19,35 +19,32 @@ prompt.get(schema, (err, result) => {
 });
 
 function shift(word) {
-	const U_SHIFT = 64;
-	const L_SHIFT = 96;
-	const U_RANGE = [64, 91];
-	const L_RANGE = [96, 123];
 	const ALPHABET = 26;
 
 	for (let offset = 1; offset <= ALPHABET; offset++) {
 		let temp = [];
 		for (let i = 0; i < word.length; i++) {
 			let code = word.charCodeAt(i);
-
-			let upper;
-			if (code > U_RANGE[0] && code < U_RANGE[1]) {
-				code -= U_SHIFT;
-				upper = true;
-			}
-			if (code > L_RANGE[0] && code < L_RANGE[1]) {
-				code -= L_SHIFT;
-				upper = false;
-			}
-
-			let newCode = code + offset;
+			let shift = getShift(code);
+			let newCode = (code - shift) + offset;
 			if (newCode > ALPHABET) newCode -= ALPHABET;
-			newCode = upper ? newCode += U_SHIFT : newCode += L_SHIFT;
-
+			newCode += shift;
 			let newLetter = String.fromCharCode(newCode);
-
 			temp[i] = newLetter;
 		}
 		console.log(`${offset}: ${temp.join('')}`);
+	}
+}
+
+function getShift(code) {
+	const U_SHIFT = 64;
+	const L_SHIFT = 96;
+	const U_RANGE = [64, 91];
+	const L_RANGE = [96, 123];
+	if (code > U_RANGE[0] && code < U_RANGE[1]) {
+		return U_SHIFT;
+	}
+	if (code > L_RANGE[0] && code < L_RANGE[1]) {
+		return L_SHIFT;
 	}
 }
